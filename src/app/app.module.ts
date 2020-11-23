816d354061c951d53ffd8c7ecd68e3c8d969ec34
Chubvasz <kareduardo@hotmail.com>
2020-11-22 19:08:09 -0600

    creando primer servicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 
+// Servicios
+import { HeroesService } from './services/heroes.service';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,7 +26,9 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     BrowserModule,
     APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService, heroesInterface } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroes',
@@ -7,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class HeroesComponent implements OnInit {
+  heroes:heroesInterface[] = [];
 
-  constructor() { }
+  constructor( private _heroesService:HeroesService ) { }
 
   ngOnInit(): void {
+    this.heroes = this._heroesService.getHeroes();
+    console.log(this.heroes);
   }
 
 }
